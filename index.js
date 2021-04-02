3cbb1bdb47f040b128832b118c46eca75e0a3880
RafaelCss <rf2014souza@gmail.com>
2021-04-02 17:14:07 -0300

    :package:script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var hamburguer = document.querySelector('.hamburguer')
+
 //add função ao menu hambuguer 
-hamburguer.addEventListener("click",e=>{
+document.querySelector('.hamburguer').addEventListener("click",e=>{
 
 document.querySelector(".container").classList.toggle("show-menu")//depois que clicar no hambuguer add uma class ...
 
